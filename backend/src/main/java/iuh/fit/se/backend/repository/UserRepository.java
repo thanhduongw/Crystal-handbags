8b9fcb46c336c5c3325e577a526fff94ff58e65e
thanhduongw <levuthanhduong2004@gmail.com>
2025-11-15 10:42:44 +0700

    Add UserRepository

---
+++ backend/src/main/java/iuh/fit/se/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package iuh.fit.se.backend.repository;
+
+import iuh.fit.se.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
