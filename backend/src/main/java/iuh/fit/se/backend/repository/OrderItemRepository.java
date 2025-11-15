7efcbfe414e7e357a0deac419cfe0df557da1fc8
thanhduongw <levuthanhduong2004@gmail.com>
2025-11-15 10:42:05 +0700

    Add OrderItemRepository

---
+++ backend/src/main/java/iuh/fit/se/backend/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package iuh.fit.se.backend.repository;
+
+import iuh.fit.se.backend.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository  extends JpaRepository<OrderItem, Integer> {
+}
