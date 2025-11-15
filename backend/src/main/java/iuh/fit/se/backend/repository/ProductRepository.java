5474076907b580c82d22e7a1dba3892a3eb7ca04
thanhduongw <levuthanhduong2004@gmail.com>
2025-11-15 10:42:34 +0700

    Add ProductRepository

---
+++ backend/src/main/java/iuh/fit/se/backend/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package iuh.fit.se.backend.repository;
+
+import iuh.fit.se.backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
