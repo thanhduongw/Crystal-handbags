1909187567834d21092773f5b43c1787874a0577
thanhduongw <levuthanhduong2004@gmail.com>
2025-11-15 10:41:45 +0700

    Add CartRepository

---
+++ backend/src/main/java/iuh/fit/se/backend/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package iuh.fit.se.backend.repository;
+
+import iuh.fit.se.backend.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository  extends JpaRepository<Cart, Integer> {
+}
