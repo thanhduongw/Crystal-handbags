b1335726bea726e450e7ae7838c2650d095b6459
thanhduongw <levuthanhduong2004@gmail.com>
2025-11-22 11:12:28 +0700

    Add ProductController

---
+++ backend/src/main/java/iuh/fit/se/backend/controller/ProductController.java
@@ -0,0 +1,32 @@
+package iuh.fit.se.backend.controller;
+
+import iuh.fit.se.backend.dto.ProductDetailDto;
+import iuh.fit.se.backend.dto.ProductListDto;
+import iuh.fit.se.backend.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/categories/{id}/products")
+    public ResponseEntity<List<ProductListDto>> getByCategory(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getByCategory(id));
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<ProductDetailDto> getDetail(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getDetail(id));
+    }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<ProductListDto>> search(@RequestParam String keyword) {
+        return ResponseEntity.ok(productService.search(keyword));
+    }
+}
