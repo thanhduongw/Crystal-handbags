bb6fd86e4c8b68607b3ef4950095ee9748d0a196
thanhduongw <levuthanhduong2004@gmail.com>
2025-11-22 11:11:23 +0700

    Update ProductItemRepository

--- backend/src/main/java/iuh/fit/se/backend/repository/ProductItemRepository.java
+++ backend/src/main/java/iuh/fit/se/backend/repository/ProductItemRepository.java
@@ -2,8 +2,14 @@ package iuh.fit.se.backend.repository;
 
 import iuh.fit.se.backend.model.ProductItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductItemRepository  extends JpaRepository<ProductItem, Long> {
+    @Query(value = "select * from product_item where product_id = :productId", nativeQuery = true)
+    List<ProductItem> findByProductId(@Param("productId") Long productId);
 }
