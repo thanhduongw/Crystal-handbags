af143e507f55ce1cc6287498a98901a7b7423665
thanhduongw <levuthanhduong2004@gmail.com>
2025-11-22 11:11:31 +0700

    Update ProductRepository

--- backend/src/main/java/iuh/fit/se/backend/repository/ProductRepository.java
+++ backend/src/main/java/iuh/fit/se/backend/repository/ProductRepository.java
@@ -2,8 +2,18 @@ package iuh.fit.se.backend.repository;
 
 import iuh.fit.se.backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p WHERE p.category.categoryId = :categoryId")
+    List<Product> findByCategoryId(@Param("categoryId") Long categoryId);
+
+    @Query("SELECT p FROM Product p WHERE LOWER(p.name) LIKE lower(concat('%', :keyword, '%')) " +
+            "OR LOWER(p.description) LIKE lower(concat('%', :keyword, '%'))")
+    List<Product> search(@Param("keyword") String keyword);
 }
