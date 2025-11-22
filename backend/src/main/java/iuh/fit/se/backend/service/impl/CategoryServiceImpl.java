3702a63d6f12db7e843ee780e99b2e52775cfbcf
thanhduongw <levuthanhduong2004@gmail.com>
2025-11-22 11:12:14 +0700

    Add CategoryServiceImpl

---
+++ backend/src/main/java/iuh/fit/se/backend/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,31 @@
+package iuh.fit.se.backend.service.impl;
+
+import iuh.fit.se.backend.dto.CategoryDto;
+import iuh.fit.se.backend.model.Category;
+import iuh.fit.se.backend.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements iuh.fit.se.backend.service.CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public List<CategoryDto> getAllCategories() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(c -> new CategoryDto(
+                        c.getCategoryId(),
+                        c.getName(),
+                        c.getImageUrl()
+                ))
+                .collect(Collectors.toList());
+    }
+}
